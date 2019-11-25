6d2f4e13803afb2247a88468b7d84be170ab4038
Terry <terryhung888@gmail.com>
2019-11-25 13:52:10 -0700

    Updated Manager class

--- src/Manager.java
+++ src/Manager.java
@@ -7,10 +7,14 @@ public class Manager extends LandLord
 	private int numOfHouses;
 	private int numOfRented;
 	private int numOfActive;
+	private String reportVal;
 	
 	public Manager(Database da) 
 	{
 		super(da);
+		numOfHouses = 0;
+		numOfRented = 0;
+		numOfActive = 0;
 	}
 	
 	public ArrayList<Property> getReport()
@@ -22,6 +26,24 @@ public class Manager extends LandLord
 	public void setPostedProperties(ArrayList<Property> report) {
 		this.propertiesReport = report;
 	}
+	
+	public void filltotals()
+	{
+		numOfHouses = propertiesReport.size();
+		for(int i = 0;i<propertiesReport.size();i++)
+		{
+			if(propertiesReport.get(i).getPropertyState() == 1 )
+				numOfActive++;
+			if(propertiesReport.get(i).getPropertyState() == 2 )
+				numOfRented++;
+		}
+		
+		reportVal = "Number of properties = " + numOfHouses + "; Number of Active = " + numOfActive
+				 	+ "; Number of Rented = " + numOfRented;
+		
+	}
+	
+	
 	
 	
 	
